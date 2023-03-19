1389ccfe0ba9ad4b454a02e680873052f60279e6
xixi <1348673083@qq.com>
2023-03-19 18:29:01 +0800

    guess number - end

--- game/game/test_2023_03_18.c
+++ game/game/test_2023_03_18.c
@@ -58,3 +58,73 @@
 //	return 0;
 //}
 
+
+
+
+//猜数字游戏
+
+void game()
+{
+	//游戏函数
+	int guess = 0;
+	int ret = rand()%100 + 1;
+	//printf("%d",ret);
+	//产生随机数
+	while (1)
+	{
+		scanf("%d", &guess);
+		if (guess > ret)
+		{
+			printf("猜大了\n");
+		}
+		else if (guess < ret)
+		{
+			printf("猜小了\n");
+		}
+		else
+		{
+			printf("恭喜你猜中了！！\n");
+			break;
+		}
+	}
+}
+
+
+void menu()
+{
+	//菜单函数
+	printf("#############################\n");
+	printf("######    1.  play     ######\n");
+	printf("######    0.  exit     ######\n");
+	printf("#############################\n");
+}
+
+int main()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	//通过时间戳来产生随机数 - 随机数的生成器
+	do
+	{
+		//进入程序，打印菜单
+		menu();
+		printf("请选择：>");
+		scanf("%d",&input);
+		//通过输入input的值来选择
+		switch(input)
+		{
+		case 1:
+			printf("游戏开始,请输入你猜测的数值（1 ~100）\n");
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("输入错误，请重新输入\n");
+			break;
+		}
+	} while (input);
+
+	return 0;
+}
