fafff45499336a91d97f8a51eeeda71d6e730f0c
xixi <1348673083@qq.com>
2023-03-19 14:02:45 +0800

    guess number

---
+++ game/game/test_2023_03_18.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <string.h>
+
+
+
+//ÉÁË¸´òÓ¡×Ö·û´®
+//int main()
+//{
+//	char arr1[] = "welcome to there!!!!";
+//	char arr2[] = "####################";
+//
+//	int left = 0;
+//	int right = strlen(arr1) - 1;
+//
+//	while (left <= right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		left++;
+//		right--;
+//		printf(arr2);
+//		Sleep(1000);
+//		system("cls");
+//	}
+//	printf(arr2);
+//
+//	return 0;
+//}
