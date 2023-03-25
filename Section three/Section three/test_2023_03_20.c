7e8714e68d9dcd9407c947e35d385fa1e66783cd
xixi <1348673083@qq.com>
2023-03-25 19:55:50 +0800

    Section three-3

--- Section three/Section three/test_2023_03_20.c
+++ Section three/Section three/test_2023_03_20.c
@@ -111,31 +111,119 @@
 //}
 
 
-int main()
-{
-	int a = 0;
-	scanf("%d", &a);//传递1234
-	//Print(a);//递归函数
-	{//第一次调用此时a=1234
-		printf("%d ", a % 10);//输出4
-		a = a / 10;//此时a=123并且传递给Print函数
-		if (a > 9)
-		{//第二次调用此时a=123
-			printf("%d ", a % 10);//输出3
-			a = a / 10;//此时a=12并且传递给Print函数
-			//Print(x / 10);
-				if (a > 9)
-				{//第三次调用此时a=12并且传递给Print函数
-					printf("%d ", a % 10);//输出2
-					a = a / 10;//此时a=1并且传递给Print函数
-					//Print(x / 10);
-					if (a > 9)
-					{//第四次调用此时a=1并且传递给Print函数
-						//Print(a/ 10);因为不满足if的条件	
-					}
-					printf("%d ", a % 10);
-				}
-		}
-	}
-	return 0;
-}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);//传递1234
+//	//Print(a);//递归函数
+//	{//第一次调用此时a=1234
+//		printf("%d ", a % 10);//输出4
+//		a = a / 10;//此时a=123并且传递给Print函数
+//		if (a > 9)
+//		{//第二次调用此时a=123
+//			printf("%d ", a % 10);//输出3
+//			a = a / 10;//此时a=12并且传递给Print函数
+//			//Print(x / 10);
+//				if (a > 9)
+//				{//第三次调用此时a=12并且传递给Print函数
+//					printf("%d ", a % 10);//输出2
+//					a = a / 10;//此时a=1并且传递给Print函数
+//					//Print(x / 10);
+//					if (a > 9)
+//					{//第四次调用此时a=1并且传递给Print函数
+//						//Print(a/ 10);因为不满足if的条件	
+//					}
+//					printf("%d ", a % 10);
+//				}
+//		}
+//	}
+//	return 0;
+//}
+
+
+//编写函数，求字符串的长度
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+
+//编写函数，不创建临时变量，求字符串的长度
+//int My_strlen(char* str)
+//{
+//	if (*str != '\0')
+//		//return 1+My_strlen(str++);
+//		//注意，这里的str++通过查看监视始终是‘a’这个字符
+//		//对于str++是先使用str然后再++（后置++）
+//		//使用的始终是str的原始数值，++没有作用
+//		//其次++str的话，str的=记录的值是改变了的，会导致遗留问题
+//		return 1+My_strlen(str+1);
+//	else
+//		return 0;
+//}
+//
+//int main()
+//{
+//	char arr[] = "abc";
+//	printf("%d\n",My_strlen(arr));
+//	return 0;
+//}
+
+
+
+//求n的阶乘
+
+//int Fac(int x)
+//{
+//	if (x <= 1)
+//		return 1;
+//	else
+//		return x*Fac(x-1);
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d",&n);
+//	printf("%d\n",Fac(n));
+//	return 0;
+//}
+
+
+//斐波那契数列
+//递归
+//int Fib(int n)
+//{
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return Fib(n - 1) + Fib(n - 2);
+//}
+////迭代
+//int Fib1(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n > 2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//
+//int main()
+//{
+//	int x = 0;
+//	scanf("%d",&x);
+//	printf("%d\n",Fib1(x));
+//	return 0;
+//}
+
