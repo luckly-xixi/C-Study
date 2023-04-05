a1c514168e140437d5819e4bf1b62d45eed3c907
xixi <1348673083@qq.com>
2023-04-05 21:43:40 +0800

    Section six-1

---
+++ Section six/Section six/test_2023_04_05.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int main()
+//{
+//	int n = 0x11223344;
+//	char* pc = &n;
+//	int* pi = &n;
+//	*pc = 0; 
+//	*pi = 0; 
+//	return 0;
+//}
