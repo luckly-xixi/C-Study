6de0c76835fe6350b5ef725dfce65562c0caa2cd
xixi <1348673083@qq.com>
2023-04-09 22:36:14 +0800

    const修饰指针的两种方式

---
+++ Section Eight/Section Eight/test_2023_04_09.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int const* p = &a;
+//	*p = 20;
+//	p = b;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int* const p = &a;
+//	p = &b;
+//	*p = b;
+//}
