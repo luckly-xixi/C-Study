74de1bdb54ea553ca1837aa073843d8409c8f450
xixi <1348673083@qq.com>
2023-04-10 22:11:44 +0800

    刷题

---
+++ Practice/Practice/test_2023_04_10.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//交换两个变量（不创建临时变量）
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("a = %d\n",a);
+//	printf("b = %d\n",b);
+//	return 0;
+//}
+
+//统计二进制中1的个数
+//int main()
+//{
+//	int a = 0;
+//	int count = 0;
+//	scanf("%d",&a);
+//	int i = 0;
+//	for (i=0; i<32; i++)
+//	{
+//		if (a & 1 == 1)
+//		{
+//			count++;
+//		}
+//		a >>= 1;
+//	}
+//	printf("%d\n",count);
+//	return 0;
+//}
+
+
