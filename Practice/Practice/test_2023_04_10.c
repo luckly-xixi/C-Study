bffc809474410802da05a10f6fa0de0c4e908ea8
xixi <1348673083@qq.com>
2023-04-18 22:52:05 +0800

    刷题

--- Practice/Practice/test_2023_04_10.c
+++ Practice/Practice/test_2023_04_10.c
@@ -34,4 +34,266 @@
 //	return 0;
 //}
 
+//输入两个整数，求两个整数二进制格式有多少个位不同
+//int main() {
+//    int a, b;
+//    scanf("%d %d", &a, &b);
+//    int x = a ^ b;
+//    int i = 0;
+//    int count = 0;
+//    for (i = 0; i < 32; i++)
+//    {
+//        if (x & 1)
+//        {
+//            count++;
+//        }
+//        x >>= 1;
+//    }
+//    printf("%d\n", count);
+//    return 0;
+//}
 
+//打印整数二进制的奇数位和偶数位
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d",&a);
+//	int i = 0;
+//	int count1 = 0;
+//	int count2 = 0;
+//	for (i = 0; i < 32; i+=2)
+//	{
+//		if ((a >> i) & 1) 
+//		{
+//			count1++;
+//		}
+//	}
+//	for (i = 1; i <= 31; i+=2)
+//	{
+//		if ((a >> i) & 1)
+//		{
+//			count2++;
+//		}
+//	}
+//	printf("奇数 = %d\n",count1);
+//	printf("偶数 = %d\n",count2);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int x = 0;
+//	scanf("%d", &x);
+//	int m = 0;
+//	int count1 = 0;
+//	int count2 = 0;
+//	for (int i = 0; i < 32; i+=2)
+//	{
+//		if (x & (x-1))
+//		{
+//			count1++;
+//		}
+//	}
+//	for (int i = 1; i < 32; i+=2)
+//	{
+//		if (x & (x - 1))
+//		{
+//			count2++;
+//		}
+//	}
+//	printf("奇数 = %d\n",count1);
+//	printf("偶数 = %d\n", count2);
+//	return 0;
+//}
+
+//KiKi想知道一个n阶方矩是否为上三角矩阵，请帮他编程判定。上三角矩阵即主对角线以下的元素都为0的矩阵，主对角线为从矩阵的左上角至右下角的连线
+//3
+//1 2 3
+//0 4 5
+//0 0 6
+//
+//int main() {
+//    int n;
+//    scanf("%d", &n);
+//    int arr[10][10];
+//    int i, j;
+//    for (i = 0; i < n; i++)
+//    {
+//        for (j = 0; j < n; j++)
+//        {
+//            scanf("%d ", &arr[i][j]);
+//        }
+//    }
+//    for (i = 0; i < n; i++)
+//    {
+//        for (j = 0; j < i; j++)
+//        {
+//
+//            if (arr[i][j] != 0)
+//            {
+//                printf("NO\n");
+//                return 0;
+//            }
+//        }
+//    }
+//    printf("YES\n");
+//    return 0;
+//}
+
+//解题思路：录入元素的同时判断是否为上三角矩阵
+//int main()
+//{
+//    int n;
+//    scanf("%d", &n); //获取n
+//    int arr[n][n];
+//    int i, j;
+//    int flag = 1;
+//    for (i = 0; i < n; i++)  //输入n*n元素
+//    {
+//        for (j = 0; j < n; j++)
+//        {
+//            scanf("%d", &arr[i][j]);
+//            if (i > j) //i>j时为下三角的元素
+//            {
+//                if (arr[i][j] != 0) { //下三角元素中有不是0的
+//                    flag = 0; //标志不为上三角矩阵                
+//                    //break 
+//                }
+//            }
+//        }
+//    }
+//    printf("%s", (flag ? "YES" : "NO"));
+//
+//    return 0;
+//}
+
+//小乐乐在课上学习了二进制八进制与十六进制后
+//对进制转换产生了浓厚的兴趣。因为他的幸运数字是6
+//所以他想知道一个数表示为六进制后的结果。请你帮助他解决这个问题。
+//void Six(int x)
+//{
+//    if (x > 5)
+//    {
+//        Six(x / 6);
+//    }
+//    printf("%d", x % 6);
+//}
+//
+//int main() {
+//    int n = 0;
+//    scanf("%d", &n);
+//    Six(n);
+//    return 0;
+//}
+
+//有一个整数序列（可能有重复的整数）
+//现删除指定的某一个整数，输出删除指定数字之后的序列
+//序列中未被删除数字的前后位置没有发生改变。
+//int main() {
+//    int n = 0;
+//    int x = 0;
+//    scanf("%d", &n);//可以换为
+//    //while(~scanf("%d",&n))因为scanf读取到几个数字就会返回数字的个数
+//    //如果scanf读取失败会返回-1，而任何整数取反都会变为该数字+1取负数（0除外，0取反是-1）
+//    //而while(scanf("%d",&n)!=EOF)当中EOF本质为-1，
+//    int arr[50];
+//    int i = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        scanf("%d ", &arr[i]);
+//    }
+//
+//    scanf("%d", &x);
+//   /* for (i = 0; i < n; i++)
+//    {
+//        if (arr[i] == x)
+//        {
+//            continue;
+//        }
+//        printf("%d ", arr[i]);
+//
+//    }*/
+//    for (i = 0; i < n; i++)
+//    {
+//        if (arr[i] != x)
+//        {
+//            printf("%d ", arr[i]);
+//        }
+//    }
+//    return 0;
+//}
+
+//实验多组输入
+//int main()
+//{
+//	int a,b,c;
+//	while (~scanf("%d %d",&a,&b))
+//	{
+//		c = a % b;
+//		printf("%d\n",c);
+//	}
+//	return 0;
+//}
+
+//小乐乐最近在课上学习了如何求两个正整数的最大公约数与最小公倍数
+//但是他竟然不会求两个正整数的最大公约数与最小公倍数之和
+//请你帮助他解决这个问题。
+
+//int main() {
+//    long long a, b;//使用long long类型放置数值过大
+//    scanf("%d %d", &a, &b);
+//    long long max, min, tmp;
+//    //辗转相除法
+//    long long n, m;//采用中间变量来避免影响求最小公倍数
+//    n = a;
+//    m = b;
+//    while (tmp = n % m)
+//    {
+//        n = m;
+//        m = tmp;
+//    }
+//    max = m;
+//    //最小公倍数=n*m/max
+//    min = a * b / max;
+//    printf("%lld", min + max);
+//    return 0;
+//}
+
+
+//小乐乐上课需要走n阶台阶
+//因为他腿比较长，所以每次可以选择走一阶或者走两阶
+//那么他一共有多少种走法？
+
+
+//斐波那契数列的思想
+//int Size(int n)
+//{
+//    if (n <= 2)
+//    {
+//        return n;
+//    }
+//    return Size(n - 2) + Size(n - 1);
+//}
+//int main() {
+//    int n = 0;
+//    scanf("%d", &n);
+//    int sum = Size(n);
+//    printf("%d", sum);
+//    return 0;
+//}
+
+
+//int main() {
+//    int fib(int i);
+//    int n = 0;
+//    scanf("%d", &n);
+//    printf("%d", fib(n));
+//    return 0;
+//}
+//int fib(int n) {//台阶为1,2时走法分别1，2种 1:(1) 2:(1,1),(2)
+//                //要走完n阶时，即我第一步要么走的1阶，要么就是2阶，   eg:      上3阶 
+//                //若第一步是走1阶的时候，那么还剩n-1个台阶          第一步：走1阶，剩余2阶
+//                //若第一步是走2阶的时候，那么还剩n-2个台阶          第一步：走2阶，剩余1阶
+//                //即转变为求我走1阶剩余台阶的走法+走2阶时剩余台阶的走法，即可递推到走最后一步的走法有多少种
+//    return n <= 2 ? n : fib(n - 1) + fib(n - 2);
+//}
