3819fa0844e65fe66c037a8d109fd1f2a244612a
xixi <1348673083@qq.com>
2023-05-17 23:42:38 +0800

    库函数的使用

--- Section Eight/Section Eight/test_2023_04_09.c
+++ Section Eight/Section Eight/test_2023_04_09.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<assert.h>
 
 //int main()
 //{
@@ -18,3 +19,44 @@
 //	p = &b;
 //	*p = b;
 //}
+
+
+//stelen的使用和熟悉：
+
+
+//int main()
+//{
+//	//char* p1 = "abcdef"; - 6
+//	//char arr1[] = "abcdef"; - 6
+//	//int len = strlen(p1);
+//	//int len = strlen(arr1);
+//	//char arr2[] = {'a','b','c'};
+//	//int len = strlen(arr2); - 随机值
+//
+//	//char* p2 = 'a';
+//	//int len = strlen(p2); - #error
+//	printf("%d\n",len);
+//	return 0;
+//}
+
+
+//模拟实现stelen
+
+int my_strlen(const char* str)
+{
+	assert(str);
+	int count = 0;
+	while (str++)
+	{
+		count++;
+	}
+	return count;
+}
+
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d\n",len);
+	return 0;
+}
