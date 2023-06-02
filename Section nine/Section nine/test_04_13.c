33c5cc99df6cca6a2abde479112dfe22a467f209
xixi <1348673083@qq.com>
2023-06-02 23:54:38 +0800

    测试

--- Section nine/Section nine/test_04_13.c
+++ Section nine/Section nine/test_04_13.c
@@ -19,14 +19,14 @@
 
 
 //浮点数存储的例子：
-int main()
-{
-	int n = 9;
-	float* pFloat = (float*)&n;
-	printf("n的值为：%d\n", n);
-	printf("*pFloat的值为：%f\n", *pFloat);
-	*pFloat = 9.0;
-	printf("num的值为：%d\n", n);
-	printf("*pFloat的值为：%f\n", *pFloat);
-	return 0;
-}
+//int main()
+//{
+//	int n = 9;
+//	float* pFloat = (float*)&n;
+//	printf("n的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n", *pFloat);
+//	*pFloat = 9.0;
+//	printf("num的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n", *pFloat);
+//	return 0;
+//}
