689d60a252431bd882105a532553b85d5b2cb6a3
xixi <1348673083@qq.com>
2023-06-08 23:27:44 +0800

    malloc、calloc、free

--- twelve/twelve/test_2023_06_07.c
+++ twelve/twelve/test_2023_06_07.c
@@ -7,20 +7,51 @@
 
 
 
+//int main()
+//{
+//	int* p = (int*)malloc(40);
+//	if(NULL == p)
+//	{
+//		printf("%s\n",strerror(errno));
+//	}
+//	int* ptr = p;
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		*ptr = i;
+//		ptr++;
+//	}
+//
+//	free(p);
+//	p = NULL;
+//
+//	return 0;
+//}
+
+//malloc和free的使用
+//int main()
+//{
+//	char* p = (char*)malloc(4 * sizeof(char));
+//	p = "abcd";
+//	printf("%s\n",p);
+//
+////使用过后主动释放指针并且将其置为空（NULL）
+//	free(p);
+//	p = NULL;
+//
+//
+//	return 0;
+//}
+
+
+//calloc的使用
 int main()
 {
-	int* p = (int*)malloc(40);
-	if(NULL == p)
-	{
-		printf("%s\n",strerror(errno));
-	}
-	int* ptr = p;
+	char* p = (char*)calloc(4 ,sizeof(char));
+	p = "abcd";
 
-	for (int i = 0; i < 10; i++)
-	{
-		*ptr = i;
-		ptr++;
-	}
+	for (int i = 0; i < 4; i++)
+		printf("%c",*(p+i));
 
 	free(p);
 	p = NULL;
