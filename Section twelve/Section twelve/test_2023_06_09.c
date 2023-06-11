5fb05133ea976cc405a79423929b1b44cd8dd135
xixi <1348673083@qq.com>
2023-06-11 22:43:52 +0800

    malloc、calloc、realooc、free

--- Section twelve/Section twelve/test_2023_06_09.c
+++ Section twelve/Section twelve/test_2023_06_09.c
@@ -28,35 +28,43 @@
 //	return 0;
 //}
 
-//malloc和free的使用
+//malloc和free的错误使用
 //int main()
 //{
-////	char* p = (char*)malloc(4 * sizeof(char));
-////	if (NULL == p)
-////	{
-////		perror("malloc");
-////		return 1;
-////	}
-////	p = "abcd";
-////	printf("%s\n",p);
-////
-//////使用过后主动释放指针并且将其置为空（NULL）
-////	free(p);
-////	p = NULL;
-////
-////
-////	char* ps = (char*)malloc(0);
-////	if (NULL == ps)
-////	{
-////		perror("malloc");
-////		return 1;
-////	}
-////	ps = "a";
-////	free(ps);
-////	ps = NULL;
+//	char* p = (char*)malloc(4 * sizeof(char));
+//	if (NULL == p)
+//	{
+//		perror("malloc");
+//		return 1;
+//	}
+//
+//	//错位位置，因为对于指针p的错误使用
+//	p = "abcd";
+//	printf("%s\n",p);
+//
+////使用过后主动释放指针并且将其置为空（null）
+//	free(p);
+//	p = NULL;
+//
+//	return 0;
+//}
+
+
+//malloc和free的正确使用
+//int main()
+//{
+//	char* ps = (char*)malloc(0);
+//	if (NULL == ps)
+//	{
+//		perror("malloc");
+//		return 1;
+//	}
+//	ps = "a";
+//	free(ps);
+//	ps = NULL;
 //
 //
-//	int* p = (int*)malloc(4*sizeof(int));
+//	int* p = (int*)malloc(4 * sizeof(int));
 //	if (NULL == p)
 //	{
 //		perror("malloc");
@@ -69,13 +77,10 @@
 //
 //	for (int i = 0; i < 4; i++)
 //	{
-//		printf("%d\n",*(p+i));
+//		printf("%d\n", *(p + i));
 //	}
-//
-//
 //	return 0;
 //}
-
 
 //calloc的使用
 //int main()
@@ -125,6 +130,40 @@
 //realloc的使用
 //int main()
 //{
+//	int* p = (int*)malloc(5 * sizeof(int));
+//	if (NULL == p)
+//	{
+//		printf("开辟空间失败\n");
+//		return 1;
+//	}
+//	for (int i = 0; i < 5; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//
+//	for (int i= 0; i < 5; i++)
+//	{
+//		printf("%d\n",*(p+i));
+//	}
+//	//调整空间
+//	p = (int*)realloc(p, 8 * sizeof(int));
+//	if (NULL == p)
+//	{
+//		printf("开辟空间失败\n");
+//		return 1;
+//	}
+//	for (int i = 0; i < 8; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//
+//	for (int i = 0; i < 8; i++)
+//	{
+//		printf("%d\n", *(p + i));
+//	}
+//	//释放
+//	free(p);
+//	p = NULL;
 //
 //	return 0;
 //}
