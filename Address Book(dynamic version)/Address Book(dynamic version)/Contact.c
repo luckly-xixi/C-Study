24d74f41854283d5b8426c520872bc7c9ceb2d61
xixi <1348673083@qq.com>
2023-06-15 23:22:08 +0800

    通讯录（动态版本）未完成

---
+++ Address Book(dynamic version)/Address Book(dynamic version)/Contact.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"Contact.h"
+
+//²Ëµ¥
+void menu()
+{
+	printf("******************************\n");
+	printf("****  1.add      2.del    ****\n");
+	printf("****  3.search   4.modify ****\n");
+	printf("****  5.show     6.sort   ****\n");
+	printf("****       0.exit         ****\n");
+	printf("******************************\n");
+}
---
+++ Address Book(dynamic version)/Address Book(dynamic version)/Contact.h
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+#define MAX_NAME 20
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 30
+
+
+
+struct PeoInfo 
+{
+	char name[MAX_NAME];
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	int age;
+	char addr[MAX_ADDR];
+};
+
+struct Contact
+{
+	struct PeoInfo data[MAX];
+	int sz;
+};
+
+
+//菜单
+void menu();
+
+//初始化通讯录
+void InitConcatc(struct Contact* ps);
+
+//增加通讯录
+void AddContact(struct Contact* ps);
+
+//删除通讯录成员
+void DelContact(struct Contact* ps);
+
+//查找联系人
+void SearchContact(struct Contact* ps);
+
+//修改联系人
+void ModifyContact(struct Contact* ps);
+
+//通讯录排序
+void SortContact(struct Contact* ps);
+
+//打印展示
+void ShowContact(const struct Contact* ps);
