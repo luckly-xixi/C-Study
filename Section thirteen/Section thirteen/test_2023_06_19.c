863f09357ebb48289cdade031dcba4a2b03d4419
xixi <1348673083@qq.com>
2023-06-21 23:58:19 +0800

    文件的打开和关闭

--- Section thirteen/Section thirteen/test_2023_06_19.c
+++ Section thirteen/Section thirteen/test_2023_06_19.c
@@ -2,7 +2,22 @@
 
 #include<stdio.h>
 
+
+
+//文件打开和关闭
 int main()
 {
+	FILE* pf;
+	//打开文件
+	pf = fopen("text.txt","w");
+	if (NULL == pf)
+	{
+		perror("fopen");
+	}
+
+
+	//关闭文件
+	fcolse(pf);
+
 	return 0;
 }
