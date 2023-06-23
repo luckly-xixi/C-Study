72a7a09a321f2a4a1d89f524f9c48eec1566a426
xixi <1348673083@qq.com>
2023-06-23 23:43:06 +0800

    scanf\sscanf\fscanf和printf\sprintf\sprintf的区别

--- Section thirteen/Section thirteen/test_2023_06_19.c
+++ Section thirteen/Section thirteen/test_2023_06_19.c
@@ -71,19 +71,19 @@
 //	}
 //
 //
-//	//读文件
-//	//for (int i = 0; i < 26; i++)
-//	//{
-//	//	int ch = fgetc(pf);
-//	//	printf("%c",ch);
-//	//}
-//
-//
-//	int ch = 0;
-//	while ((ch = fgetc(pf))!= EOF)
-//	{
-//		printf("%c", ch);
-//	}
+	//读文件
+	//for (int i = 0; i < 26; i++)
+	//{
+	//	int ch = fgetc(pf);
+	//	printf("%c",ch);
+	//}
+
+
+	//int ch = 0;
+	//while ((ch = fgetc(pf))!= EOF)
+	//{
+	//	printf("%c", ch);
+	//}
 //
 //
 //	//关闭文件
@@ -104,13 +104,13 @@
 //		return 1;
 //	}
 //	
-//	//fputs("hello",pf);
-//	//fputs("world",pf);
-// //会自动在字符串后面放一个' \0 '
-// //并且在写文件的时候不在字符串中主动放一个换行转义字符的话，并不会在文件当中主动换行
-// //多次输入的时候默认在字符串末尾去加上之后加的相对应的字符串
-//	fputs("hello\n", pf);
-//	fputs("world", pf);
+	////fputs("hello",pf);
+	////fputs("world",pf);
+ ////会自动在字符串后面放一个' \0 '
+ ////并且在写文件的时候不在字符串中主动放一个换行转义字符的话，并不会在文件当中主动换行
+ ////多次输入的时候默认在字符串末尾去加上之后加的相对应的字符串
+	//fputs("hello\n", pf);
+	//fputs("world", pf);
 //
 //	fclose(pf);
 //	pf = NULL;
@@ -130,10 +130,10 @@
 //	}
 //		fputs("hello",pf);
 //
-//	char arr[20]="**********";
-//	fgets(arr,20,pf);
-//	//读取的时候会将文件里的换行和空格读取打印，并且，一次读取遇见换行默认结束
-//	printf("%s",arr);
+	//char arr[20]="**********";
+	//fgets(arr,20,pf);
+	////读取的时候会将文件里的换行和空格读取打印，并且，一次读取遇见换行默认结束
+	//printf("%s",arr);
 //
 //	fclose(pf);
 //	pf = NULL;
@@ -144,12 +144,12 @@
 
 
 //格式化的输入与输出
-//struct S
-//{
-//	char name[20];
-//	int age;
-//	float hight;
-//};
+struct S
+{
+	char name[20];
+	int age;
+	float hight;
+};
 //格式化写文件
 //int main()
 //{
@@ -161,7 +161,7 @@
 //		return 1;
 //	}
 //	
-//	fprintf(pf,"%s %d %.1f",s.name,s.age,s.hight );
+	//fprintf(pf,"%s %d %.1f",s.name,s.age,s.hight );
 //	
 //	fclose(pf);
 //	pf = NULL;
@@ -181,11 +181,72 @@
 //		return 1;
 //	}
 //
-//	fscanf(pf,"%s %d %.1f",s.name,&(s.age),&(s.hight));
-//	printf("%s %d %.1f", s.name, s.age, s.hight);
+	//fscanf(pf,"%s %d %.1f",s.name,&(s.age),&(s.hight));
+	//printf("%s %d %.1f", s.name, s.age, s.hight);
 //
 //	fclose(pf);
 //	pf = NULL;
 //	return 0;
 //}
 
+
+//二进制输入
+//int main()
+//{
+//	struct S s = {"zhang san",24,180.2f};
+//	FILE* pf = fopen("test.txt","wb");
+//	if (NULL == pf)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//	
+	//fwrite(&s,sizeof(s),1,pf);
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+
+//二进制输出
+//int main()
+//{
+//	struct S s = { 0 };
+//	FILE* pf = fopen("test.txt", "rb");
+//	if (NULL == pf)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//
+	//fread(&s, sizeof(s), 1, pf);
+	//printf("%s %d %f",s.name,s.age,s.hight);
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+
+//sscanf的使用 
+//int main()
+//{
+//	struct S s = { "zhang san",25,180.3f };
+//	struct S tmp = { 0 };
+//	char buf[100] = { 0 };
+//	//把结构体数据转换为字符串
+//	sprintf(buf,"%s %d %f",s.name,s.age,s.hight);
+//	printf("%s\n",buf);
+//
+//	//把字符串转化为结构体数据
+//	sscanf(buf, "%s %d %f", tmp.name, &(tmp.age), &(tmp.hight));
+//	printf("%s %d %f", tmp.name, tmp.age, tmp.hight);
+//
+//	return 0;
+//}
+
+
+
+
+//sprintf的使用
